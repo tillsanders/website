f301df23b3c4224ee17785e4fc32a9ead76d788f
Till Sanders <mail@till-sanders.de>
2020-12-15 01:35:44 +0100

    fix(font): add Fira Code
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/main.css'],
+  css: ['~/assets/main.scss', '@openfonts/fira-code_latin/index.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
